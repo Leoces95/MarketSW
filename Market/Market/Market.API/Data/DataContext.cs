822463301b9d69c7e07c0b2ce0b13c8b1f554a25
LEONEL ANTONIO MARTINEZ SILGADO <leonelmartinez321289@correo.itm.edu.co>
2023-09-19 15:08:51 -0500

    Countries migration

---
+++ Market/Market/Market.API/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+using Market.Share.Entities;
+
+namespace Market.API.Data
+{
+    public class DataContext:DbContext
+    {
+        // Utilizar las propiedades de DbContext
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {
+            
+        }
+        // se hace un DbSet por cada entidad/tabla creada
+        public DbSet <Country> Countries { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ Market/Market/Market.Share/Entities/Country.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market.Share.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; } // primary key, identity (1,1)
+
+        [Display (Name = "Pais")] // Etiquetas del nombre del campo
+        [MaxLength (100, ErrorMessage = "El campo {0} debe contener {1}")] // es la longitud de caracteres del campo o del atributo
+        // se puede personalizar el mensaje
+        [Required (ErrorMessage = "El campo {0} es obligatorio")]
+        public string? Name { get; set; } // ? (indica que hace un salto de nulos)
+
+        
+        
+    }
+}
